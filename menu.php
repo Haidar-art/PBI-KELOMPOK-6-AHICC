b242ddfda76666f68ee1d5183a9f73b17b1edc3a
ibnuqubaisy <105423318+ibnuqubaisy@users.noreply.github.com>
2022-05-24 16:28:05 +0700

    Add files via upload
    saya di sini membuat menu atau tampilan menu dan data base untuk pencarian

---
+++ menu.php
@@ -0,0 +1,17 @@
+    <!--Navbar-->
+    <nav class="navbar navbar-default">
+        <div class="container">
+
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="daftar.php">Daftar</a></li>
+                <li><a href="simpan.php">Simpan</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+            
+            <form action="pencarian.php" method= "get" class="navbar -form navbar-right ">
+                <input type="text" class="form - control" name="keyword">
+                <button class ="btn btn-primary">Cari</button>
+        </div>
+    </nav>
---
+++ pencarian.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+//koneksi ke database
+$koneksi = new mysqli("localhost", "root", "", "incafe");
+?>
+<?php
+$keyword = $_GET["keyword"];
+
+$semuadata=array();
+$ambil = $koneksi->query("SELECT * FROM tambah_cafe WHERE nama_toko_cafe LIKE '%$keyword%' 
+    OR deskripsi_cafe LIKE '%%$keyword%'");
+    while($pecah = $ambil->fetch_assoc())
+    {
+        $semuadata[]=$pecah;
+    }
+
+//echo "<pre>";
+//print_r ($semuadata);
+//echo "</pre>";
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>InCafe</title>
+    <link href="admin/assets/css/bootstrap.css" rel="stylesheet" />
+</head>
+
+<body>
+<?php include 'menu.php' ; ?>
+    <div class="container">
+        <h3>Hasil Pencarian : <?echo $keyword ?></h3>
+
+        <?php if (empty($semuadata)):?>
+            <div class="alert alert-danger">Cafe <strong><?php echo $keyword?></strong> Pencarian tidak ditemukan</div>
+            <?php endif ?> 
+        
+        <div class="row">
+
+        <?php foreach ($semuadata as $key => $value):?>
+
+        <div class="col-md-3">
+            <div class="thumbnail">
+                <img src="fotocafe/<?php echo $value["foto"] ?>"alt="" class="img-responsive">
+                <div class="caption">
+                    <h3><?php echo $value["nama_toko_cafe"] ?></h3>
+                    <a href="detail.php?id=<?php echo $value ["id_tambah_cafe"];?> "class="btn btn-default">Detail</a>
+                </div>
+            </div>
+        </div>
+        <?php endforeach ?>
+        </div>
+    </div>
+</body>
+</html>
